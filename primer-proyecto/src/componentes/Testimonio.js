751debcc51b01608eefeea548aa33d49fe41b48d
israel <isramr2000@gmail.com>
2023-04-16 16:00:43 -0600

    Finalizacion del proyecto responsive

--- primer-proyecto/src/componentes/Testimonio.js
+++ primer-proyecto/src/componentes/Testimonio.js
@@ -4,12 +4,16 @@ import '../stylesheets/Testimonio.css';
 function Testimonio(props){
   return (
     <div className="contenedor-testimonio">
-      <img 
-      className='imagen-testimonio'
-      src={require(`../imagenes/testimonio-${props.imagen}.jpg`) }
-      alt='Foto de Goku'/>
+      <div className='contenedor-imagen'>
+        <img 
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.jpg`) }
+        alt='Foto de Goku'/>  
+      </div>
+      
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>{props.nombre} de {props.planeta}</p>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> de {props.planeta}</p>
         <p className='cargo-testimonio'>{props.cargo} en {props.fase}</p>
         <p className='texto-testimonio'>{props.testimonio}</p>
       </div>
