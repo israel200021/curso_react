8dc9381d627e4827acf635762438fff30f4d93ff
israel <isramr2000@gmail.com>
2023-04-16 00:48:59 -0600

    Finalizacion de componente de testimonios

--- primer-proyecto/src/App.js
+++ primer-proyecto/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+        <h1>Esto tienen que decir estos personajes</h1>
+        <Testimonio 
+        nombre = 'Gohan'
+        planeta = 'univeros 7'
+        imagen = 'gohan'
+        cargo = 'Guerrero sayajin'
+        fase = 'Fase 2'
+        testimonio = '“¡Mi padre es el mejor luchador de todos los tiempos … y quiero pelear con la misma ropa que él!”'
+        />
+        <Testimonio 
+        nombre = 'Trunks'
+        planeta = 'univeros 7'
+        imagen = 'trunks'
+        cargo = 'Guerrero sayajin'
+        fase = 'Fase 1'
+        testimonio = '“Padre, eres tan duro y orgulloso, duro y frío como una roca. Pero, aun así, en el fondo, tu corazón late como el mío. Sé que te sientes. Soy tu hijo y siempre te amaré. Me alegro de poder verte, papá.” '
+        />
+
+      <Testimonio 
+        nombre = 'Goku'
+        planeta = 'univeros 7'
+        imagen = 'goku'
+        cargo = 'Guerrero sayajin'
+        fase = 'Fase 2'
+        testimonio = 'Peleaste muy bien, te felicito. Por favor, hijo, dile a tu mamá que me disculpe, siempre hice las cosas a mi manera, sin hacerle caso…'
+        />
+      </div>
+      
     </div>
   );
 }
--- primer-proyecto/src/componentes/Testimonio.js
+++ primer-proyecto/src/componentes/Testimonio.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import '../stylesheets/Testimonio.css';
 
-function Testimono(){
+function Testimonio(props){
   return (
     <div className="contenedor-testimonio">
       <img 
       className='imagen-testimonio'
-      src='' />
+      src={require(`../imagenes/testimonio-${props.imagen}.jpg`) }
+      alt='Foto de Goku'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>{props.nombre} de {props.planeta}</p>
+        <p className='cargo-testimonio'>{props.cargo} en {props.fase}</p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
+      </div>
     </div>
   );
 }
+
+export default Testimonio;
